5fe71c637399c857c1785957c9564513268a13e3
fullmoonawoo <110724763+fullmoonawoo@users.noreply.github.com>
2024-03-19 09:28:55 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,6 +2,6 @@ from transFORM import Transform
 
 
 
-perform = Transform("source.txt", "Data-Result", separator=":", position=3, jump=2)
+perform = Transform("source.txt", "Data-Result", delimiter=",", separator=":", position=3, jump=2)
 #perform.transform()
 print(perform.show_data())
